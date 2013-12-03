5e512c567eda0a5b5bda5b9ee303482c681143b2
Yujun ZHANG <yujun.zhang@schneider-electric.com>
2013-12-03 10:50:53 +0800

    Add type Entry

---
+++ test/entry_test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Entry = require('../lib/entry');
+
+describe('Entry', function () {
+  var entry;
+  beforeEach(function () {
+    entry = new Entry();
+  });
+  it('should have strings and context', function () {
+    entry.strings.should.be.an.instanceof(Object);
+    entry.context.should.be.an.instanceof(Array);
+  });
+  it('should add string', function () {
+    entry.lang('en', 'hello');
+    entry.strings.should.eql({
+      'en': 'hello'
+    });
+  });
+  it('should add context', function () {
+    entry.lang('en', 'hello').ctx('fusion', {group: 1, label: 1});
+    entry.should.eql({
+      'strings': {
+        'en': 'hello'
+      },
+      'context': [{
+        'type': 'fusion',
+        'data': {
+          'group': 1,
+          'label': 1
+        }
+      }]
+    });
+  });
+});
