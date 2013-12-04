c2192cebeddc20e829431f9469ddf379d8f94c18
Yujun ZHANG <yujun.zhang@schneider-electric.com>
2013-12-04 13:15:05 +0800

    Refactor test case

--- test/plugins/fusion_printer_test.js
+++ test/plugins/fusion_printer_test.js
@@ -2,79 +2,84 @@
 
 var printer = require('../../lib/plugins/fusion/printer');
 
-describe('LabelBuilder', function () {
-  var label = new printer.LabelBuilder('1', 'hello');
-  it('should export data', function () {
-    label.data().should.eql({
-      '$': {
-        'id': '1'
-      },
-      '_': 'hello'
+describe('Printer', function () {
+  var label, group, lang;
+
+  beforeEach(function () {
+    lang = new printer.LangBuilder('en');
+    group = new printer.GroupBuilder('1');
+    label = new printer.LabelBuilder('2', 'hello');
+
+    group.addLabel(label);
+    lang.addGroup(group);
+  });
+
+  describe('LabelBuilder', function () {
+    it('should export data', function () {
+      label.data().should.eql({
+        '$': {
+          'id': '2'
+        },
+        '_': 'hello'
+      });
     });
   });
-});
 
-describe('GroupBuilder', function () {
-  var group = new printer.GroupBuilder('1');
-  var label = new printer.LabelBuilder('2', 'hello');
-  group.addLabel(label);
-  it('should export data', function () {
-    group.data().should.eql({
-      '$': {
-        'id': '1'
-      },
-      'Label': [
-        {
-          '$': {
-            'id': '2'
-          },
-          '_': 'hello'
-        }
-      ]
+  describe('GroupBuilder', function () {
+    it('should export data', function () {
+      group.data().should.eql({
+        '$': {
+          'id': '1'
+        },
+        'Label': [
+          {
+            '$': {
+              'id': '2'
+            },
+            '_': 'hello'
+          }
+        ]
+      });
+    });
+    it('should find existing label', function () {
+      group.findLabel('2').should.eql(label);
+    });
+    it('should create new label when not found', function () {
+      var newLabel = new printer.LabelBuilder('3', 'world');
+      group.findLabel('3', function () {
+        return newLabel;
+      }).should.eql(newLabel);
     });
   });
-  it('should find existing label', function () {
-    group.findLabel('2').should.eql(label);
-  });
-  it('should create new label when not found', function () {
-    var newLabel = new printer.LabelBuilder('3', 'world');
-    group.findLabel('3', function () {
-      return newLabel;
-    }).should.eql(newLabel);
-  });
-});
 
-describe('LangBuilder', function () {
-  var lang = new printer.LangBuilder('en');
-  var group = new printer.GroupBuilder('1');
-  var label = new printer.LabelBuilder('2', 'hello');
-  group.addLabel(label);
-  lang.addGroup(group);
-  it('should export data', function () {
-    lang.data().should.eql({
-      '$': {
-        'languageId': 'en'
-      },
-      'Group': [{
+  describe('LangBuilder', function () {
+    it('should export data', function () {
+      lang.data().should.eql({
+        '$': {
+          'languageId': 'en'
+        },
+        'Group': [{
           '$': {
             'id': '1'
           },
           'Label': [{
-              '$': {
-                'id': '2'
-              },
-              '_': 'hello'
+            '$': {
+              'id': '2'
+            },
+            '_': 'hello'
           }]
-      }]
+        }]
+      });
+    });
+    it('should find group', function () {
+      lang.findGroup('1').should.eql(group);
+    });
+    it('should add new group if not found', function () {
+      var newGroup = new printer.GroupBuilder('2');
+      lang.findGroup('2', function() {
+        return newGroup;
+      }).should.eql(newGroup);
     });
   });
-  it('should find group', function () {
-    lang.findGroup('1').should.eql(group);
-  });
-  it('should add new group if not found', function () {
-    var newGroup = new printer.GroupBuilder('2');
-    lang.findGroup('2', function() {
-      return newGroup;
-    }).should.eql(newGroup);
-  });
+
 });
